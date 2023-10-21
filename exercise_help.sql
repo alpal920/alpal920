b6db5a32f61f16cd5fc41bd914dce7bfdd65e18d
alpal920 <147457604+alpal920@users.noreply.github.com>
2023-10-21 14:55:46 -0700

    Delete exercise_help.sql

--- exercise_help.sql
+++
@@ -1,9 +0,0 @@
-UPDATE exercises_test e
-JOIN functional_movements fm
-ON e.movement_id = fm.movement_id
-SET e.movement_id = (
-	SELECT movement_ID 
-    FROM functional_movements 
-    WHERE movement = 'Squat'
-)
-WHERE lower(e.name) LIKE '%squat%';
